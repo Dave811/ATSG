7e2e9ee08b75c3623842c8a306346c37fa868be9
Dave811 <d.hasshoff97@gmail.com>
2023-03-31 05:45:29 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://dave811.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/atsg',
+  baseUrl: '/ATSG',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
